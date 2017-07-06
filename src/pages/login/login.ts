b77d78323310b5db7b66a395c8b6dc10d68195d9
tetradev1 <9chakri+ds@gmail.com>
2017-07-06 10:14:50 -0700

    NAME CHANGE 1.0

---
+++ src/pages/login/login.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { NavController } from 'ionic-angular';
+
+import { UserData } from '../../providers/user-data';
+
+import { UserOptions } from '../../interfaces/user-options';
+
+import { TabsPage } from '../tabs/tabs';
+import { SignupPage } from '../signup/signup';
+
+
+@Component({
+  selector: 'page-user',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  login: UserOptions = { username: '', password: '' };
+  submitted = false;
+
+  constructor(public navCtrl: NavController, public userData: UserData) { }
+
+  onLogin(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.userData.login(this.login.username);
+      this.navCtrl.push(TabsPage);
+    }
+  }
+
+  onSignup() {
+    this.navCtrl.push(SignupPage);
+  }
+}
